2667c609388b70fe17dfce8c75bee60077050022
YumanKh <yumankhoufache607@gmail.com>
2026-05-09 19:15:43 -0500

    feat: working on SecureNotes - add addNote with AES-256 content encryption

---
+++ v2-csharp-cli/src/SecureNotes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecureVault
+{
+    public class SecureNotes
+    {
+        public struct NoteEntry
+        {
+            public string Title {  get; set; }
+            public string Content { get; set; }
+        }
+
+        public void handleSecureNotes()
+        {
+
+        }
+        public void addNote()
+        {
+            string title, content, encrypted_content;
+
+            Console.WriteLine("===Add Note===\n");
+            Console.Write("Enter note title: ");
+            title = Console.ReadLine();
+            Console.Write("Enter note content: ");
+            content = Console.ReadLine();
+
+            encrypted_content = Login.encryption.Encrypt(content);
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("notes.txt", true))
+                {
+                    writer.WriteLine($"{title}|{encrypted_content}");
+                }
+            }
+            catch (Exception) { Console.WriteLine("Error: file not found.\n"); }
+        }
+    }
+}
